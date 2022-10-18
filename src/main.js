b3cabf1f0d461cca63c4348c540b4603ec71f3e5
Pedro Luiz <67492698+httpedroluiz@users.noreply.github.com>
2022-10-18 10:27:14 -0300

    update relative links

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import "./css/index.css"
+import "../src/css/index.css"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
